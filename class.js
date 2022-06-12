ccd1cad5c3ba628c53fb51d18ea830d0fcbc55b2
AlaminDolar <amin15-9898@diu.edu.bd>
2022-06-12 18:12:16 +0600

    practise

--- class.js
+++ class.js
@@ -8,11 +8,13 @@ class Support {
         this.designation = designation;
     }
     StartSession() {
-        console.log('Start a Support Session');
+        console.log(this.name, 'Start a Support Session');
     }
 }
 
 const Amir = new Support('Amir Khan', 'BD', 'Engineer');
 const Asif = new Support('Asif Khan', 'Dubai', 'engineer');
-console.log(Amir);
-console.log(Asif);
+// console.log(Amir);
+// console.log(Asif);
+Amir.StartSession();
+Asif.StartSession();
